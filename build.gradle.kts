90d4f22fc140bb47f8eb71d2db07e21a84a89663
Madhav Baliga <42736071+mbaliga@users.noreply.github.com>
2026-08-03 14:28:58 +0530

    Use AGP built-in Kotlin support

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,4 @@
 plugins {
     id("com.android.application") version "9.3.1" apply false
-    id("com.android.library") version "9.3.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.3.21" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.3.21" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.2.0" apply false
 }
