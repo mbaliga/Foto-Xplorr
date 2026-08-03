912be8bd4d48e9048d2708be70741875e21c543c
Madhav Baliga <42736071+mbaliga@users.noreply.github.com>
2026-08-03 09:12:35 +0530

    Add Kotlin and Compose build plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
 plugins {
     id("com.android.application") version "9.3.1" apply false
+    id("com.android.library") version "9.3.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.3.21" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.3.21" apply false
 }
