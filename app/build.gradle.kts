5c3ff6ed840dc57754de02d1c28797fa8c8cadec
Madhav Baliga <42736071+mbaliga@users.noreply.github.com>
2026-08-03 17:05:14 +0530

    Fail JVM tests on accidental Android framework calls

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,10 +32,6 @@ android {
         buildConfig = true
     }
 
-    testOptions {
-        unitTests.isReturnDefaultValues = true
-    }
-
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
